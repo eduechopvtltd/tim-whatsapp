0cec8519ef145b5f4a89303c3132864b399343b9
Sahil <engineering@eduecho.com>
2026-04-20 16:42:04 +0530

    fix: remove redundant closing div tag in App component layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1187,7 +1187,6 @@ export default function App() {
                            </div>
                          )}
                       </div>
-                   </div>
                 </motion.div>
               )}
 
